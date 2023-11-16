c95e2a631de77a9063103080334574bbabdd4b68
phoebe840226 <151032266+phoebe840226@users.noreply.github.com>
2023-11-16 20:22:20 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,10 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+    "https://forms.gle/qpRPJMFf96xz8yWo9",
+    "https://forms.gle/TBJimpZ1VHJG4ipR7",
+    "https://forms.gle/EiZs83zUPQ1kssum8",
+    "https://forms.gle/YSo2wYxLvJc26rfn9",
+    "https://forms.gle/sg1t4HirRdK2NcrZ7",
+    "https://forms.gle/7GjSjFe8jwiWxnpZ7"
 ];
