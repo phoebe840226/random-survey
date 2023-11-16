f1b5d66c9063c5bdde4f2ae38eb2c8497c341504
phoebe840226 <151032266+phoebe840226@users.noreply.github.com>
2023-11-16 20:29:52 +0800

    Update url.js

--- url.js
+++ url.js
@@ -5,13 +5,11 @@
  * 1. 除最後一項外，其餘項目用逗號分隔
  * 2. 網址請用引號包起來
  */
-
-
 const urls = [
-    "https://forms.gle/qpRPJMFf96xz8yWo9",
-    "https://forms.gle/TBJimpZ1VHJG4ipR7",
-    "https://forms.gle/EiZs83zUPQ1kssum8",
-    "https://forms.gle/YSo2wYxLvJc26rfn9",
-    "https://forms.gle/sg1t4HirRdK2NcrZ7",
-    "https://forms.gle/7GjSjFe8jwiWxnpZ7"
+    'https://forms.gle/qpRPJMFf96xz8yWo9',
+    'https://forms.gle/TBJimpZ1VHJG4ipR7',
+    'https://forms.gle/EiZs83zUPQ1kssum8',
+    'https://forms.gle/YSo2wYxLvJc26rfn9',
+    'https://forms.gle/sg1t4HirRdK2NcrZ7',
+    'https://forms.gle/7GjSjFe8jwiWxnpZ7'
 ];
