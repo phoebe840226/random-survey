fb4561fc9099dcf081ff4101e6ba4dce27c4339a
phoebe840226 <151032266+phoebe840226@users.noreply.github.com>
2023-11-19 20:30:57 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 const urls = [
     "https://forms.gle/qpRPJMFf96xz8yWo9",
     "https://forms.gle/TBJimpZ1VHJG4ipR7",
-    "https://forms.gle/EiZs83zUPQ1kssum8",
     "https://forms.gle/YSo2wYxLvJc26rfn9",
     "https://forms.gle/sg1t4HirRdK2NcrZ7",
     "https://forms.gle/7GjSjFe8jwiWxnpZ7"
