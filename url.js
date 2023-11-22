618a3e87d3fa7653f4b377451f81a41bd34936c1
phoebe840226 <151032266+phoebe840226@users.noreply.github.com>
2023-11-22 08:27:22 +0800

    Update url.js

--- url.js
+++ url.js
@@ -1,4 +1,4 @@
-/*
+ /*
  * 將要隨機派發的網址放入下方（不需排序）
  *
  * 並請注意：
@@ -6,7 +6,6 @@
  * 2. 網址請用引號包起來
  */
 const urls = [
-    "https://forms.gle/RHPRCZ9cc34DTrjZ7",
     "https://forms.gle/A5vSwXBmZQY6tCsv5",
     "https://forms.gle/68waXyviKJRK3m8R6",
     "https://forms.gle/VoGynTWF7dap3qnP8"
