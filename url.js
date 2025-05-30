7c4ff10c24e3f2f20187c3f3da7ba24d8dde747a
phoebe840226 <151032266+phoebe840226@users.noreply.github.com>
2025-05-30 23:47:58 +0800

    Update url.js
    Signed-off-by: phoebe840226 <151032266+phoebe840226@users.noreply.github.com>

--- url.js
+++ url.js
@@ -1,6 +1,16 @@
 const urls = [
+        "https://forms.gle/ekKUbvEPvM9ZYtqT7",
+        "https://forms.gle/ptMGZRFc8RpEVmpt8",
+        "https://forms.gle/GHiRTfmjKyZW8qBm7",
+        "https://forms.gle/N62SWcuuSr8DmLZf6",
+        "https://forms.gle/egDoL3BozLW7jsRv8",
         "https://forms.gle/4Encjmp1yjBnsiSH9",
+        "https://forms.gle/GnzjBMv2ptnsvvAi9",
         "https://forms.gle/5fW1CzyvLsC5W13a6",
+        "https://forms.gle/f9oCTqvTP2gLAkbP8",
+        "https://forms.gle/EvtgYvpkBEi7iqZ17",
+        "https://forms.gle/LWwK1wYz7CQShPiv9",
+        "https://forms.gle/UUzEw1C5voQUoZTP6",
 ];
       
 
