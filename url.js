c11cdc79706f9ffd8925044fb8583bd28b8d69ec
phoebe840226 <151032266+phoebe840226@users.noreply.github.com>
2025-06-06 10:47:23 +0800

    Update url.js
    Signed-off-by: phoebe840226 <151032266+phoebe840226@users.noreply.github.com>

--- url.js
+++ url.js
@@ -2,7 +2,6 @@ const urls = [
         "https://forms.gle/f9oCTqvTP2gLAkbP8",
         "https://forms.gle/EvtgYvpkBEi7iqZ17",
         "https://forms.gle/LWwK1wYz7CQShPiv9",
-        "https://forms.gle/UUzEw1C5voQUoZTP6",
 ];
       
 
